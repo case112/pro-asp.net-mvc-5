7e94f29529e1c316e79aab553cf2f244cc5d2719
case112 <case112@users.noreply.github.com>
2021-05-26 09:42:33 +0300

    Add edit and delete products functionality

--- SportsStore/SportsStore.Domain/Entities/Product.cs
+++ SportsStore/SportsStore.Domain/Entities/Product.cs
@@ -19,3 +19,24 @@ namespace SportsStore.Domain.Entities
         public string Category { set; get; }
     }
 }
+
+// Should be with validation but did not want to migrate
+//public class Product
+//{
+//    [HiddenInput(DisplayValue = false)]
+//    public int ProductID { get; set; }
+
+//    [Required(ErrorMessage = "Please enter a product name")]
+//    public string Name { get; set; }
+
+//    [Required(ErrorMessage = "Please enter a description")]
+//    public string Description { get; set; }
+
+//    [Required]
+//    [Range(0.01, double.MaxValue, ErrorMessage = "Please enter a positive price")]
+//    public decimal Price { get; set; }
+
+//    [Required(ErrorMessage = "Please specify a category")]
+//    public string Category { set; get; }
+//}
+
--- SportsStore/SportsStore.UnitTests/AdminTests.cs
+++ SportsStore/SportsStore.UnitTests/AdminTests.cs
@@ -121,6 +121,29 @@ namespace SportsStore.UnitTests
             Assert.IsInstanceOfType(result, typeof(ViewResult));
         }
 
+        [TestMethod]
+        public void Can_Delete_Valid_Products()
+        {
+
+            // arrange
+            Product prod = new Product { ProductID = 2, Name = "Test" };
+
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[] {
+                new Product {ProductID = 1, Name = "P1"},
+                prod,
+                new Product {ProductID = 3, Name = "P3"},
+            });
+
+            AdminController target = new AdminController(mock.Object);
+
+            // act
+            target.Delete(prod.ProductID);
+
+            // assert - ensure that the repository delete method was 
+            // called with the correct Product 
+            mock.Verify(m => m.DeleteProduct(prod.ProductID));
+        }
     }
 
 }
