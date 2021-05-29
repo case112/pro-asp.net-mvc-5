e70d674799e17d80d50d3f7db3368ae616b3a9dc
case112 <case112@users.noreply.github.com>
2021-05-29 17:03:16 +0300

    Add image upload option to admin view

--- SportsStore/SportsStore.Domain/Entities/Product.cs
+++ SportsStore/SportsStore.Domain/Entities/Product.cs
@@ -12,31 +12,22 @@ namespace SportsStore.Domain.Entities
     {
         [HiddenInput(DisplayValue = false)]
         public int ProductID { get; set; }
+
+        [Required(ErrorMessage = "Please enter a product name")]
         public string Name { get; set; }
-        [DataType(DataType.MultilineText)]
+
+        [Required(ErrorMessage = "Please enter a description")]
         public string Description { get; set; }
+
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Please enter a positive price")]
         public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Please specify a category")]
         public string Category { set; get; }
+
+        public byte[] ImageData { get; set; }
+        public string ImageMimeType { get; set; }
     }
 }
-
-// Should be with validation but did not want to migrate
-//public class Product
-//{
-//    [HiddenInput(DisplayValue = false)]
-//    public int ProductID { get; set; }
-
-//    [Required(ErrorMessage = "Please enter a product name")]
-//    public string Name { get; set; }
-
-//    [Required(ErrorMessage = "Please enter a description")]
-//    public string Description { get; set; }
-
-//    [Required]
-//    [Range(0.01, double.MaxValue, ErrorMessage = "Please enter a positive price")]
-//    public decimal Price { get; set; }
-
-//    [Required(ErrorMessage = "Please specify a category")]
-//    public string Category { set; get; }
-//}
 
