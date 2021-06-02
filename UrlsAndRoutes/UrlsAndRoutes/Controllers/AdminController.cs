60acc856279e734edf40eca1ca04102a77611b20
case112 <case112@users.noreply.github.com>
2021-06-02 18:46:53 +0300

    creating example controllers

---
+++ UrlsAndRoutes/UrlsAndRoutes/Controllers/AdminController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UrlsAndRoutes.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            ViewBag.Controller = "Index";
+            ViewBag.Action = "Index";
+            return View("ActionName");
+        }
+    }
+}
